f15ec1812ed323f3b9594de96bd814581f968e95
kerilpatel <kerilpatel@outlook.com>
2024-06-01 13:30:41 +0530

    feat: tech skills section

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter } from "react-router-dom";
 import {About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas} from './components';
 
 const App = () => {
-
 	return (
 		<BrowserRouter>
 		<div className="relative z-0 bg-primary">
@@ -13,6 +12,7 @@ const App = () => {
 			</div>
 			<About />
 			<Experience />
+			<Tech />
 		</div>
 		</BrowserRouter>
 	)
