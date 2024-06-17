04588a2d0168cc769d674fe9db06f4b8ef1fb09a
kerilpatel <kerilpatel@outlook.com>
2024-06-17 20:04:57 +0530

    feat: contact form

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,10 @@ const App = () => {
 			<Tech />
 			<Works />
 			<Feedbacks />
+			<div className='relative z-0'>
+				<Contact />
+				<StarsCanvas />
+			</div>
 		</div>
 		</BrowserRouter>
 	)
