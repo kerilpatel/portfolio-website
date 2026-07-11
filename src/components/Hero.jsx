f81767bf3905b3b554f5bdbd211e31dd289cf6bb
Keril <kerilpatel@outlook.com>
2026-07-12 00:27:11 +0530

    feat: updated hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
             Hi, I'm <span className="text-[#915eff]">Keril</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            Software Developer
+            Software Engineer
           </p>
         </div>
       </div>
