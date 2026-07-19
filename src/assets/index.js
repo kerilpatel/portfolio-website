84414a79f0df253a7e62f860adf21348bf1c4b6e
Keril <kerilpatel@outlook.com>
2026-07-19 19:31:07 +0530

    feat: add live screenshots to project cards, drop latex tag

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,9 +1,12 @@
 import backend from "./backend.png";
+import claudeMarketplace from "./claude-marketplace.png";
 import close from "./close.svg";
 import college from "./college.png";
 import creator from "./creator.png";
 import github from "./github.png";
+import logator from "./logator.png";
 import logo from "./logo.svg";
+import lumen from "./lumen.png";
 import menu from "./menu.svg";
 import mobile from "./mobile.png";
 import nvidiaGenAiBadge from "./nvidia-genai-badge.png";
@@ -26,4 +29,7 @@ export {
   school,
   nvidiaGenAiBadge,
   projectPlaceholder,
+  lumen,
+  logator,
+  claudeMarketplace,
 };
